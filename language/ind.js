6f19a7d83989238e4eed74f207953b5656355f15
CLEITIN20 <77171065+CLEITIN20@users.noreply.github.com>
2021-02-01 22:10:54 -0300

    Add files via upload

---
+++ language/ind.js
@@ -0,0 +1,278 @@
+exports.wait = () => {
+	return`*「 ESPERAR 」 MÉDIO*`
+}
+
+exports.succes = () => {
+	return`*「 SUCESSOS 」*`
+}
+
+exports.lvlon = () => {
+	return`*「 HABILITAR 」 NIVELANDO*`
+}
+
+exports.lvloff = () => {
+	return`*「 DESATIVAR 」NIVELAMENTO*`
+}
+
+exports.lvlnul = () => {
+	return`*SEU NÍVEL AINDA ESTÁ BAIXO*`
+}
+
+exports.lvlnoon = () => {
+	return`*O NÍVEL DO GRUPO NÃO FOI ATIVADO*`
+}
+
+exports.noregis = () => {
+	return`*「 NÃO REGISTRADO 」*\n\n*Como registrar no bot do henrique${prefix}registrar nome|idade* \n*exemplo ${prefix}registrar henrique|16*`
+}
+
+exports.rediregis = () => {
+	return`*「 JÁ REGISTRADO 」*\n\n*você já está registrado no banco de dados do bot*`
+}
+
+exports.stikga = () => {
+	return`*sim falhou, tente repetir mais alguns momentos*`
+}
+
+exports.linkga = () => {
+	return`*desculpe link inválido*`
+}
+
+exports.groupo = () => {
+	return`*「SÓ EM GRUPOS」*`
+}
+
+exports.ownerb = () => {
+	return`*「APENAS O DONO DO BOT」*`
+}
+
+exports.ownerg = () => {
+	return`*「SOMENTE GRUPO DE PROPRIETÁRIOS」*`
+}
+
+exports.admin = () => {
+	return`*「SOMENTE ADMS DO GRUPO」*`
+}
+
+exports.badmin = () => {
+	return`*「O BOT DEVE SER ADMINISTRADOR」*`
+}
+
+exports.nsfwoff = () => {
+	return`*NSFW EM ATIVO*`
+}
+
+exports.bug = () => {
+	return`*Problemas foram relatados ao proprietário do BOT, relatórios falsos não serão respondidos*`
+}
+
+exports.wrongf = () => {
+	return`*Formato incorreto / texto em branco*`
+}
+
+exports.clears = () => {
+	return`*limpar todo o sucesso*`
+}
+
+exports.pc = () => {
+	return`*「 CADASTRO 」*\n\nuntuk para saber se você se registrou verifique a mensagem que enviei \n\nNOTE:\n*jika você não entendeu a mensagem significa que você não salvou o número do seu bot*`
+}
+
+exports.registered = (namaUser, umurUser, serialUser, time, sender) => {
+	return`*「 DADOS DE PAÍS 」*\n\nvocê se registrou com dados \n\n┏━⊱nome\n┗⊱${namaUser}\n┏━⊱seu numero\n┗⊱wa.me/${sender.split("@")[0]}\n┏━⊱sua idade\n┗⊱${umurUser}\n┏━⊱hora de registro\n┗⊱${time}\n\n┏━❉ *NS* ❉━\n┣⊱${serialUser}\n┗⊱NOTA : não se esqueça deste número porque é importante:v`
+}
+
+exports.cmdnf = (prefix, command) => {
+	return`comando *${prefix}${command}* não encontrado \ tente escrever *${prefix}menu*`
+}
+
+exports.owneresce = (pushname) => {
+	return`*Desculpe, mas ${pushname} não o script do dono*`
+}
+
+exports.reglevelaha = (command, pushname, getLevelingLevel, sender, aha) => {
+	return`*Desculpe ${pushname} seu nível não é suficiente*\n\n*┏⊱seu nivel : ${getLevelingLevel(sender)}*\n*┣⊱tipo de comando : ${command}*\n*┗⊱requisitos de nível : ${aha}*\n\n_NOTA: CHAT / SEMPRE PARA RECEBER XP_`
+}
+
+exports.reglevelahb = (command, pushname, getLevelingLevel, sender, ahb) => {
+	return`*Desculpe ${pushname} seu nível não é suficiente*\n\n*┏⊱seu nivel : ${getLevelingLevel(sender)}*\n*┣⊱tipo de comando : ${command}*\n*┗⊱requisitos de nível : ${aha}*\n\n_NOTA: CHAT / SEMPRE PARA RECEBER XP_`
+}
+
+exports.reglevelahc = (command, pushname, getLevelingLevel, sender, ahc) => {
+	return`*Desculpe ${pushname} seu nível não é suficiente*\n\n*┏⊱seu nivel : ${getLevelingLevel(sender)}*\n*┣⊱tipo de comando : ${command}*\n*┗⊱requisitos de nível : ${aha}*\n\n_NOTA: CHAT / SEMPRE PARA RECEBER XP_`
+}
+
+exports.reglevelahd = (command, pushname, getLevelingLevel, sender, ahd) => {
+	return`*Desculpe ${pushname} seu nível não é suficiente*\n\n*┏⊱seu nivel : ${getLevelingLevel(sender)}*\n*┣⊱tipo de comando : ${command}*\n*┗⊱requisitos de nível : ${aha}*\n\n_NOTA: CHAT / SEMPRE PARA RECEBER XP_`
+}
+
+exports.reglevelahe = (command, pushname, getLevelingLevel, sender, ahe) => {
+	return`*Desculpe ${pushname} seu nível não é suficiente*\n\n*┏⊱seu nivel : ${getLevelingLevel(sender)}*\n*┣⊱tipo de comando : ${command}*\n*┗⊱requisitos de nível : ${aha}*\n\n_NOTA: CHAT / SEMPRE PARA RECEBER XP_`
+}
+
+exports.reglevelahf = (command, pushname, getLevelingLevel, sender, ahf) => {
+	return`*Desculpe ${pushname} seu nível não é suficiente*\n\n*┏⊱seu nivel : ${getLevelingLevel(sender)}*\n*┣⊱tipo de comando : ${command}*\n*┗⊱requisitos de nível : ${aha}*\n\n_NOTA: CHAT / SEMPRE PARA RECEBER XP_`
+}
+
+exports.menu = (pushname, prefix, getLevelingLevel, getLevelingXp, sender, reqXp, _registered, uangku, role) => { 
+	return `
+┏━━━⊱  *HENRIQUE BOTS*  ⊰━━┓
+┣⊱ *Nome* : ${pushname}
+┣⊱ *Número* : wa.me/${sender.split("@")[0]}
+┣⊱ *o seu rp* : Rp${uangku}
+┣⊱ *XP* : ${getLevelingXp(sender)}/${reqXp}
+┣⊱ *Função* : ${role}
+┣⊱ *Nível* : ${getLevelingLevel(sender)}
+┣⊱ *Daftar pengguna* : ${_registered.length}
+┗━━━⊱  ⸨ *HENRIQUE BOTS* ⸩  ⊰━━━━┛
+╭─────────────
+│DONO DO BOT
+│
+│𝐇𝐄𝐍𝐑𝐈𝐐𝐔𝐄 𝐁𝐎𝐓𝐒
+│
+│CONTATO DO MEU DONO
+│
+│https://wa.me/+5527998401752
+│
+│GRUPO DO DONO DO BOT
+│
+│ https://chat.whatsapp.com/D6pzWbHSxnPFImv410f2p3
+│
+│ ADICIONA O BOT EM GRUPOS
+╰─────────────
+
+┏━━⊱ *MAKER MENU* ⊰━━┓
+┣⊱ *${prefix}dono*
+┣⊱ *${prefix}sticker* 
+┣⊱ *${prefix}avengers* <teks|teks>
+┣⊱ *${prefix}summer* <teks>
+┣⊱ *${prefix}sandwrite* <teks>
+┣⊱ *${prefix}metaldark* <teks>
+┣⊱ *${prefix}dropwater* <teks>
+┣⊱ *${prefix}greenneon* <teks>
+┣⊱ *${prefix}neontext* <teks>
+┣⊱ *${prefix}sumery* <teks>
+┣⊱ *${prefix}blood* <teks>
+┣⊱ *${prefix}firework* <teks>
+┣⊱ *${prefix}lava* <teks>
+┣⊱ *${prefix}qrcode* <teks>
+┣━━⊱  *FUN MENU*  ⊰━━━┫
+┣⊱ *${prefix}mining*
+┣⊱ *${prefix}bisakah* <teks>
+┣⊱ *${prefix}kapankah* <teks>
+┣⊱ *${prefix}apakah* <teks>
+┣⊱ *${prefix}rate* <teks>
+┣⊱ *${prefix}slap*
+┣⊱ *${prefix}tampar*
+┣⊱ *${prefix}moddroid* <teks>
+┣⊱ *${prefix}happymod* <teks>
+┣⊱ *${prefix}nangis*
+┣⊱ *${prefix}cium*
+┣⊱ *${prefix}peluk*
+┣━━⊱ *MUTUAL* ⊰━━━━━┫
+┣⊱ *${prefix}mutual*
+┣⊱ *${prefix}next*
+┣━━⊱ *MEDIA MENU* ⊰━━┫
+┣⊱ *${prefix}brainly* [error]
+┣⊱ *${prefix}pinterest*
+┣⊱ *${prefix}resepmasakan* <teks>
+┣⊱ *${prefix}igstalk* <username>
+┣⊱ *${prefix}bitly* <link>
+┣━━⊱ *YT & SONG* ⊰━━━┫
+┣⊱ *${prefix}ytmp3* <link>
+┣⊱ *${prefix}ytmp4* <link>
+┣⊱ *${prefix}joox* <judul>
+┣━━⊱  *NSFW MENU*  ⊰━┫
+┣⊱ *${prefix}anjing*
+┣⊱ *${prefix}blowjob*
+┣⊱ *${prefix}nekonime*
+┣⊱ *${prefix}husbu*
+┣⊱ *${prefix}ranime*
+┣━━⊱ *LIMIT & UANG* ⊰━┫
+┣⊱ *${prefix}limit* 
+┣⊱ *${prefix}buylimit* <jumblah>
+┣⊱ *${prefix}transfer* <tag |jumblah>
+┣⊱ *${prefix}dompet*
+┣━━⊱ *GROUP MENU* ⊰━┫
+┣⊱ *${prefix}hidetag*
+┣⊱ *${prefix}grouplist*
+┣⊱ *${prefix}level*
+┣⊱ *${prefix}linkgc*
+┣⊱ *${prefix}tagall*
+┣⊱ *${prefix}setpp*
+┣⊱ *${prefix}add* <tag>
+┣⊱ *${prefix}kick* <tag>
+┣⊱ *${prefix}setname* <teks>
+┣⊱ *${prefix}setdesc* <teks>
+┣⊱ *${prefix}demote* <tag>
+┣⊱ *${prefix}promote* <tag>
+┣⊱ *${prefix}listadmin*
+┣⊱ *${prefix}group* [buka/tutup]
+┣⊱ *${prefix}leveling* [enable/disable]
+┣⊱ *${prefix}nsfw* [1/0]
+┣⊱ *${prefix}simih* [1/0]
+┣⊱ *${prefix}welcome* [1/0]
+┣━━⊱ *OWNER MENU* ⊰━┫
+┣⊱ *${prefix}bc* <teks>
+┣⊱ *${prefix}bcgc* <teks>
+┣⊱ *${prefix}kickall* <rawan ban>
+┣⊱ *${prefix}setreply* <teks>
+┣⊱ *${prefix}setprefix* <symbol>
+┣⊱ *${prefix}clearall*
+┣⊱ *${prefix}block* <tag>
+┣⊱ *${prefix}unblock* <tag>
+┣⊱ *${prefix}leave*
+┣⊱ *${prefix}event* [1/0]
+┣⊱ *${prefix}clone* <tag>
+┣⊱ *${prefix}setppbot*
+┣━━⊱ *imfo* ⊰━━┫
+┃
+┣
+│DONO DO BOT
+│
+│𝐇𝐄𝐍𝐑𝐈𝐐𝐔𝐄 𝐁𝐎𝐓𝐒
+│
+│CONTATO DO MEU DONO
+│
+│https://wa.me/+5527998401752
+│
+│GRUPO DO DONO DO BOT
+│
+│ https://chat.whatsapp.com/D6pzWbHSxnPFImv410f2p3
+│
+│ ADICIONA O BOT EM GRUPOS
+┃
+┗━━⊱  ⸨ ** ⸩  ⊰━━━━┛
+`
+}
+
+exports.levelup = (pushname, sender, getLevelingXp,  getLevel, getLevelingLevel, role) => {
+	return`
+*「 FELIZ 」*
+┏⊱ *Nome* : ${pushname}
+┣⊱ *Número* : wa.me/${sender.split("@")[0]}
+┣⊱ *Xp* : ${getLevelingXp(sender)}
+┣⊱ *Limite* : +3
+┣⊱ *Função* :  ${role}
+┗⊱ *Level* : ${getLevel} ⊱ ${getLevelingLevel(sender)}
+`}
+ 
+exports.limitend = (pushname) => {
+	return`*maaf ${pushname} limit hari ini habis*\n*beli limit untuk mendapatkan limit/ naik level*`
+}
+
+exports.limitcount = (limitCounts) => {
+	return`
+*「 LIMIT COUNT 」*
+sisa limit anda : ${limitCounts}
+
+NOTE : untuk mendapatkan limit. bisa lewat naik level atau buylimit`
+}
+
+exports.satukos = () => {
+	return`*Tambah parameter 1/enable atau 0/disable`
+}
+
+exports.uangkau = (pushname, sender, uangkau) => {
+	return`*┏⊱ 「 ATM 」⊰━┓*\n┣⊱ *Nome* : ${pushname}\n┣⊱ *Número* : ${sender.split("@")[0]}\n┣⊱ *rp* : ${uangkau}\n┗━━━━━━━━━━`
+}
